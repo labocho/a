49f0e69581a65cb710e6336eefb031326ba2ee03
labocho <labocho@penguinlab.jp>
2017-12-07 19:43:36 +0900

    use es6 class

--- Rakefile
+++ Rakefile
@@ -1,3 +1,7 @@
 task "compile" do
   sh "babel", "-o", "dist/application.js", "src/application.js"
 end
+
+task "watch" do
+  sh "babel", "-w", "-o", "dist/application.js", "src/application.js"
+end
--- src/application.js
+++ src/application.js
@@ -1,48 +1,87 @@
-// document.addEventListener("DOMContentLoaded", function() {
 (function(){
-  var context = new (window["AudioContext"] || window["webkitAudioContext"])();
-  var gain = context.createGain();
-  var osc = context.createOscillator();
-  var isPlaying = false;
+  class Voice {
+    constructor(props) {
+      this.context = props.context;
+      this.frequency = props.frequency || Voice.DEFAULT_VALUES.frequency;
+      this.volume = props.volume || Voice.DEFAULT_VALUES.volume;
+      this.attack = props.attack || Voice.DEFAULT_VALUES.attack;
+      this.release = props.release || Voice.DEFAULT_VALUES.release;
+      this.isPlaying = false;
+    }
 
-  var volume = 0.5;
-  var frequency = 440;
-  var attack = 0.02;
-  var release = 0.1;
+    play() {
+      const now = this.context.currentTime;
 
-  osc.frequency.value = 440;
-  osc.connect(gain);
+      this.gain = this.context.createGain();
+      this.gain.gain.setValueAtTime(0, now);
+      this.gain.gain.linearRampToValueAtTime(this.volume, now + this.attack);
+      this.gain.connect(this.context.destination);
 
-  gain.gain.setValueAtTime(0, context.currentTime);
-  gain.connect(context.destination);
+      this.osc = this.context.createOscillator();
+      this.osc.frequency.value = this.frequency;
+      this.osc.connect(this.gain);
 
+      this.osc.start(now);
 
-  play = function() {
-    now = context.currentTime
-    gain.gain.setValueAtTime(0, now);
-    gain.gain.linearRampToValueAtTime(volume, now + attack);
-    osc.start(now);
-    isPlaying = true;
-  }
+      this.isPlaying = true;
+    }
 
-  stop = function() {
-    now = context.currentTime
-    gain.gain.cancelScheduledValues(now);
-    gain.gain.setValueAtTime(gain.gain.value, now)
-    gain.gain.linearRampToValueAtTime(0, now + release);
-    osc.stop(now + release);
-    isPlaying = false;
-  }
+    stop() {
+      const now = this.context.currentTime
+      this.gain.gain.cancelScheduledValues(now);
+      this.gain.gain.setValueAtTime(this.gain.gain.value, now)
+      this.gain.gain.linearRampToValueAtTime(0, now + this.release);
+      this.osc.addEventListener("ended", ()=> {
+        this.gain.disconnect(this.context);
+      });
+      this.osc.stop(now + this.release);
 
-  toggle = function() {
-    console.log(isPlaying);
-    if (isPlaying) {
-      stop();
-    } else {
-      play();
+      this.isPlaying = false;
+    }
+
+    toggle() {
+      if (this.isPlaying) {
+        this.stop();
+      } else {
+        this.play();
+      }
     }
   }
 
-  document.querySelector("#playButton").addEventListener("touchstart", toggle);
-  // document.querySelector("#playButton").addEventListener("mousedown", toggle);
+  Voice.DEFAULT_VALUES = {
+    volume: 0.5,
+    frequency: 440,
+    attack: 0.02,
+    release: 0.1,
+  };
+
+
+  class Application {
+    run() {
+      this.context = new (window["AudioContext"] || window["webkitAudioContext"])();
+      this.voice = null;
+
+      document.querySelector("#playButton").addEventListener("touchstart", this.toggle.bind(this));
+    }
+
+    play() {
+      this.voice = new Voice({context: this.context});
+      this.voice.play();
+    }
+
+    stop() {
+      this.voice.stop();
+      this.voice = null;
+    }
+
+    toggle() {
+      if (this.voice) {
+        this.stop()
+      } else {
+        this.play()
+      }
+    }
+  }
+
+  new Application().run();
 }());
